91255ca11372923d45d41801292f16ea1121d8d8
LYagamitop1 <adarshnayak432@gmail.com>
2026-09-13 10:31:46 +0530

    Added code for emotion detector

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
-# Emotion-Detector-YOLO
+# Emotion Detector YOLO
+
+## This Emotion detector can detect over 9 different emotions:
+## Natural, Happy, Sad, Sleepy, Surprised, Angry, Contempt, Disgust, Fear
+
+## I trained a yolo model using dataset of over 1,00,000 images of peoples showing these emotions. 
+
+## It took about 12 Hours to complete 50 epochs for the training of nano model of yolo using Nvidia Tesla T4 x2 GPU.
+
+
+## Enter "q" to exit the real time video capture
---
+++ emotion_detector.py
@@ -0,0 +1,44 @@
+from ultralytics import YOLO 
+import cv2
+import cvzone
+import math
+
+
+cap = cv2.VideoCapture(0)
+
+model = YOLO("./yolo-weights/best.pt")
+
+classNames = ['angry', 'contempt', 'disgust', 'fear', 'happy', 'natural', 'sad', 'sleepy', 'surprised']
+
+while True:
+    success, img = cap.read()
+    img = cv2.resize(img, (960, 540))
+    results = model(img, stream=True)
+    
+    for r in results:
+        boxes = r.boxes
+        
+        for box in boxes:
+            x1, y1, x2, y2 = box.xyxy[0]
+            x1, y1, x2, y2 = int(x1), int(y1), int(x2), int(y2)
+            
+            cv2.rectangle(img, (x1,y1), (x2,y2), (255,0,255), 3)
+            
+            w, h = x2-x1, y2-y1
+            bbox = x1, y1, w, h
+            
+            cvzone.cornerRect(img, bbox)
+            conf = box.conf[0]
+            conf = math.ceil(conf*100)/100
+            
+            cls = int(box.cls[0])
+            
+            cvzone.putTextRect(img, f"{classNames[cls]} {conf}", (max(0,x1),max(30,y1-20)),scale = 1.2, offset=5, colorR=(255,255,255), colorT=(0,0,0))
+    
+
+    cv2.imshow("YOLO Real-Time Detection", img)
+    if cv2.waitKey(1) & 0xFF == ord("q"):
+        break
+
+cap.release()
+cv2.destroyAllWindows()
